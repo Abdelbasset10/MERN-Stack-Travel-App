bb5de1045da3ce8c2df74a3ac134b991ad41f27a
Rezazi Abdelbasset <basset.rezazi26@gmail.com>
2022-12-25 11:25:28 +0100

    .

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const {SignUp,SignIn} = require('../controllers/auth')
-
-router.post('/sign-up',SignUp)
-router.post('/sign-in',SignIn)
-
-module.exports = router
